15d2243ded7c2584fc4f234dcc9acb7229907e95
Filip Janik <FilipAJanik@gmail.com>
2024-02-07 08:35:06 +0000

    Updated the Java class to App instead of Main

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,7 +2,7 @@ package com.napier.sem;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class App {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
