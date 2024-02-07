bb5c0071d407d1709876617d1d044bde82af864a
Filip Janik <FilipAJanik@gmail.com>
2024-02-07 08:43:53 +0000

    Added an App.java to stay on track with the Lab guides

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,6 +1,6 @@
 package com.napier.sem;
-public class App {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("App Test...");
+        System.out.println("Hello World...");
     }
 }
