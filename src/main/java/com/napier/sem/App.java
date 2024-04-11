7bfb0d8f8c6c396f6eade16898fafdf31318c4ae
maja4h <veraluna4046@gmail.com>
2024-04-11 14:32:56 +0100

    Lab3a connecting Database to Application. Not working. Fix Docker Compose.yml

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,71 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
+//adding MYSQL server code to start up the database
+
+import java.sql.*;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            //Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        //Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+
+            try
+            {
+                //Wait a bit for db to start
+                Thread.sleep(30000);
+                //Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                //Wait a bit
+                Thread.sleep(10000);
+                //Exit for loop
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+
+        if (con !=null)
+        {
+            try
+            {
+                //Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+
+            }
+        }
+    }
+}
+
+/* import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
@@ -27,3 +92,4 @@ public class App {
 
     }
 }
+ */
