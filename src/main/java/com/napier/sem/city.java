d54b54436d3de4f1aeedb9327aebdabeb13ecb97
Alice <alicenurmcconnell@gmail.com>
2024-04-18 12:03:16 +0100

    Creating the classes

---
+++ src/main/java/com/napier/sem/city.java
@@ -0,0 +1,32 @@
+package com.napier.sem;
+
+public class city {
+
+    public class city {
+        /**
+         * city ID
+         */
+        public int ID;
+
+        /**
+         * City Name
+         */
+        public char Name;
+
+        /**
+         * CountryCode
+         */
+        public char CountryCode;
+
+        /**
+         * District
+         */
+        public char District;
+
+        /**
+         * Population
+         */
+        public int Population;
+
+    }
+}
---
+++ src/main/java/com/napier/sem/country.java
@@ -0,0 +1,38 @@
+package com.napier.sem;
+
+public class country {
+        /**
+         * Country Code
+         */
+        public int Code;
+
+        /**
+         * Name of country
+         */
+        public String Name;
+        /**
+         * Name of continent
+         */
+        public String Continent;
+
+        /**
+         * Name of Region
+         */
+        public String Region;
+
+        /**
+         * SurfaceArea
+         */
+        public int SurfaceArea;
+
+        /**
+         * IndepYear
+         */
+        public int IndepYear;
+
+        /**
+         * Population
+         */
+        public int Population;
+    }
+}
